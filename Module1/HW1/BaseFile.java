d97c6494e9d3ae29dea4e5955fbb1edae66f594e
Maksym Mazurkevych <maksym_mazurkevych@epam.com>
2017-09-21 12:06:58 +0300

    Added_otherOperations

--- Module1/HW1/BaseFile.java
+++ Module1/HW1/BaseFile.java
@@ -5,4 +5,16 @@ public class BaseFile {
     private int add(int a, int b){
         return a+b;
     }
+
+    private static double div(int a, int b){
+        return (double)(a/b);
+    }
+
+   private static int sub(int a, int b){
+        return a-b;
+   }
+
+   private static int multipl(int a, int b){
+       return a*b;
+   }
 }
