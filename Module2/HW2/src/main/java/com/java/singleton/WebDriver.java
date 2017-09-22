0a211a1a8d95c0893c25e3f79e929c251113afac
Maksym Mazurkevych <maksym_mazurkevych@epam.com>
2017-09-22 10:24:09 +0300

    Removed_webdriver_fields

--- Module2/HW2/src/main/java/com/java/singleton/WebDriver.java
+++ Module2/HW2/src/main/java/com/java/singleton/WebDriver.java
@@ -3,8 +3,6 @@ package com.java.singleton;
 public class WebDriver {
 
     private int speed;
-    private String firstName;
-    private String lastName;
 
     public int getSpeed() {
         return speed;
@@ -12,14 +10,6 @@ public class WebDriver {
 
     public void setSpeed(int speed) {
         this.speed = speed;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
 
 }
