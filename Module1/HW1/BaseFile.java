bfcfe2cf273c8bd4b5013a3db9c1f8ed92274c11
Maksym Mazurkevych <maksym_mazurkevych@epam.com>
2017-10-04 14:27:35 +0300

    Added realization with 3 parameters

--- Module1/HW1/BaseFile.java
+++ Module1/HW1/BaseFile.java
@@ -1,8 +1,13 @@
 public class BaseFile {
 
     public int add(int a, int b){
-        System.out.println("This is add method");
+        System.out.println("This is add method with 2 parameters");
         return a+b;
+    }
+
+    public int add(int a, int b, int c){
+        System.out.println("This is add method  with 3 parameters");
+        return a+b+c;
     }
 
     public int sub(int a, int b){
