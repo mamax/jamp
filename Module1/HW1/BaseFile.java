7edcdfb68fa1589fbd37240ead81a8540fe53830
Maksym Mazurkevych <maksym_mazurkevych@epam.com>
2017-10-04 13:42:15 +0300

    Added subtraction operation realization

--- Module1/HW1/BaseFile.java
+++ Module1/HW1/BaseFile.java
@@ -1,7 +1,11 @@
 public class BaseFile {
 
-    private int add(int a, int b){
+    public int add(int a, int b){
         return a+b;
+    }
+
+    public int sub(int a, int b){
+        return a-b;
     }
 
 }
