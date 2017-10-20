37fa492695c9fbf18c97e286b1fa2fc09b780235
Maksym Mazurkevych <maksym_mazurkevych@epam.com>
2017-10-20 11:39:37 +0300

    Implemented  methods with java8ParallelAggregator

--- Module1/HW3/src/main/java/com/epam/cdp/m2/hw2/aggregator/Java8ParallelAggregator.java
+++ Module1/HW3/src/main/java/com/epam/cdp/m2/hw2/aggregator/Java8ParallelAggregator.java
@@ -1,23 +1,38 @@
 package com.epam.cdp.m2.hw2.aggregator;
 
-import java.util.List;
-
 import javafx.util.Pair;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Java8ParallelAggregator implements Aggregator {
 
     @Override
     public int sum(List<Integer> numbers) {
-        throw new UnsupportedOperationException();
+        return numbers.parallelStream().mapToInt(Integer::intValue).sum();
     }
 
     @Override
     public List<Pair<String, Long>> getMostFrequentWords(List<String> words, long limit) {
-        throw new UnsupportedOperationException();
+        return words.parallelStream()
+                .map(i -> new Pair<>(i, (long) Collections.frequency(words, i)))
+                .distinct()
+                .sorted(UtilsAggregator.PAIRS_COMPARATOR)
+                .limit(limit)
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<String> getDuplicates(List<String> words, long limit) {
-        throw new UnsupportedOperationException();
+        final Set<String> all = new HashSet<>();
+
+        return words.parallelStream().
+                map(String::toLowerCase).
+                filter(n -> !all.add(n)).
+                limit(limit).
+                collect(Collectors.toList());
     }
 }
