8f3d38c31eeab14182fad929d47664419e856ba0
Maksym Mazurkevych <maksym_mazurkevych@epam.com>
2017-10-25 15:30:47 +0300

    Implemented write-behind LFU cache

--- Module1/HW4/caches/src/main/java/com/caches/cache/CacheFactory.java
+++ Module1/HW4/caches/src/main/java/com/caches/cache/CacheFactory.java
@@ -5,7 +5,7 @@ import com.caches.dao.CrudDao;
 public class CacheFactory {
 
     public <KEY, VAL> Cache<KEY, VAL> createEntityCache(int maxSize, CrudDao<KEY, VAL> systemOfRecord) {
-        return null; // TODO implement
+        return new ListFirstUsedCache<KEY, VAL>(maxSize, systemOfRecord);
     }
 
 }
